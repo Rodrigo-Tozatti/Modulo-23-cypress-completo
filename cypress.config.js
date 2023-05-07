283fa480b06c26caba65f849c4a83e234093a5d2
Rodrigo_Tozatti <rodrigo.tozatti@hotmail.com>
2023-05-07 19:56:30 -0300

    Relatorios

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'reporters/custom.js',
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+  
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
