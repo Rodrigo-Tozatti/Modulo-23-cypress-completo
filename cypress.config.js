055a0ba1ac88b0dafeb36620f575622ca3e19629
Rodrigo_Tozatti <rodrigo.tozatti@hotmail.com>
2023-06-19 16:43:05 -0300

    Relatórios concluido

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'reporters/custom.js',
-  reporter: 'mochawesome',
-  reporterOptions: {
-    reportDir: 'cypress/results',
-    overwrite: false,
-    html: false,
-    json: true,
-  },
-  
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "mochawesome-report",
+      overwrite: false,
+      reportFilename: "index",
+      html: true,
+      json: false,
+      //timestamp: "dd-mm-yyyy_HH-MM",
+    },
+
     baseUrl: 'http://lojaebac.ebaconline.art.br/'
   },
 });
